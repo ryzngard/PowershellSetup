4d9bcff9951250887d1517886ac48093ac75dc87
Andrew Hall <ryzngard@live.com>
2019-06-05 23:01:50 -0700

    Flush out install.ps1

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# PowershellSetup
+## PowershellSetup
+
 My personal powershell environment setup. All of the following I use:
 
 * [Scoop](https://scoop.sh/)
@@ -6,10 +7,20 @@ My personal powershell environment setup. All of the following I use:
 * [Powerline](https://github.com/Jaykul/PowerLine)
 * [Powerline fonts](https://github.com/powerline/fonts)
 * [PSGit](https://github.com/PoshCode/PSGit)
+* [PSReadline](https://docs.microsoft.com/en-us/powershell/module/psreadline/?view=powershell-6)
+* [Terminal](https://github.com/Microsoft/Terminal)
 * Scripts from the scripts folder + auto load of those scripts
 
-# To Use
+## To Use
+
+Make sure submodules are synced using `git submodule update --init` 
 
 Run install.ps1. Requires [Powershell 6](https://aka.ms/ps6)
 
 You can also follow the links to understand how to install each component individually
+
+## Useful added commands
+
+* `which` - similar to which in bash, unfolds the definition of something. `which which` will show the command definition of itself
+* `$__` - displays the last output. See the `Out-Default.ps1` for more details
+* `ctrl+r` will search through history of commands
--- install.ps1
+++ install.ps1
@@ -1,44 +1,68 @@
-function Install-Scoop
-{
+Install-Hub 
+Install-Powerline 
+Install-PowerlineFonts
+Install-PSGit 
+Install-Profile
+
+# Installation function definition 
+
+function Install-Scoop {
     iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 }
 
-function Requires-Scoop
-{
-    try 
-    {
+function Requires-Scoop {
+    try {
         Get-Command scoop 
         return $True
     }
-    catch 
-    {
+    catch {
         Install-Scoop
     }
 }
 
-function Install-Hub
-{
+function Install-Hub {
     Requires-Scoop
 
     scoop install hub
 }
 
-function Install-Powerline 
-{
+function Install-Powerline {
+    install-module powerline -Scope CurrentUser -AllowClobber -Confirm
+    $powerlineConfig = Get-ResourcePath "Powerline_Configuration.psd1" 
+    $destination = Join-Path $env:AppData "powershell/HuddledMasses.org/Powerline/Configuration.psd1"
 
+    Ensure-Directory $destination 
+
+    Move-Item $powerlineConfig $destination
 }
 
-function Install-PowerlineFonts
-{
-
+function Install-PowerlineFonts {
+    $fontsScript = Get-ResourcePath "fonts/install.ps1"
+    & $fontsScript
 }
 
-function Install-PSGit 
-{
-
+function Install-PSGit {
+    Install-Module psgit -Scope CurrentUser -AllowClobber -Confirm
 }
 
-function Install-Profile 
-{
-    
+function Install-Profile {
+    $profilePath = Get-ResourcePath "Profile.ps1"
+    Move-Item $profilePath $profile 
+}
+
+function Get-ScriptDirectory {
+    Split-Path -parent $PSCommandPath
+}
+
+function Get-ResourcePath($relativePath) {
+    return Join-Path Get-ScriptDirectory $relativePath
+}
+
+function Ensure-Directory($path) {
+    $directory = Split-Path -Parent $path
+    if (Test-Path $directory) {
+        return 
+    }
+
+    New-Item -ItemType Directory $directory
 }
