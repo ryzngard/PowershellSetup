033b35f0ddf8a82d22ecc959e155207eecd8b4a1
Andrew Hall <ryzngard@live.com>
2023-08-11 13:23:08 -0700

    Updated to latest settings

--- Profile.ps1
+++ Profile.ps1
@@ -1,19 +1,23 @@
 # Default Modules
 Import-Module PScolor
 Import-Module posh-docker
-Import-Module PowerLine
-Import-Module PSGit
 Import-Module posh-git
 
-Import-Module PSReadLine
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -EditMode Windows
+#oh-my-posh setup
+oh-my-posh init pwsh --config C:\Users\ryzng\OneDrive\Documents\WindowsPowerShell\darkblood.omp.json | Invoke-Expression
+$env:POSH_GIT_ENABLED = $true
 
-oh-my-posh init pwsh --config "$((get-item $PROFILE).DirectoryName)\darkblood.omp.json" | Invoke-Expression
+#PS ReadLine setup
+set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -colors @{ ListPrediction = '#8A0303' }
 
-# Set up colors
-[console]::BackgroundColor = "black"
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+
+    # Set up colors
+    [console]::BackgroundColor = "black"
+}
 
 # Global functions
 $profileDirectory = (Get-Item $PROFILE).Directory
@@ -23,5 +27,3 @@ gci "$profileDirectory\Scripts" -Filter "*.ps1" | %{
     Write-Host "Loading $($_.FullName)"
     . $_.FullName
 }
-
-
